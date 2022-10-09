8e6dd11d59ebaeda56f2d8ce109595f34d2e8124
Grzesiek <grzesiek138@onet.eu>
2022-10-09 14:53:08 +0200

    bufix/Test sql fixed

--- src/test/java/pl/niepracuj/controller/AdvertisementControllerIT.java
+++ src/test/java/pl/niepracuj/controller/AdvertisementControllerIT.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.web.servlet.MockMvc;
 import pl.niepracuj.model.dto.advertisement.AdvertisementSearchCriteriaDto;
 import pl.niepracuj.model.enums.SeniorityEnum;
@@ -27,38 +28,40 @@ import static pl.niepracuj.util.TestUtils.toJson;
 //TODO Do naprawy ładowanie danych wejściowych
 @SpringBootTest
 @AutoConfigureMockMvc
+@SqlGroup({
+        @Sql(scripts = "/sql/controller/advertisement.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+        @Sql(scripts = "/sql/controller/advertisement-cleanup.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
 public class AdvertisementControllerIT {
 
     @Autowired
     private MockMvc mockMvc;
 
-//    @Test
-//    @Sql("/sql/controller/advertisement.sql")
-//    public void whenGetAllAdvertisements_thenReturnAdvertisements() throws Exception {
-//        // when && then
-//        mockMvc.perform(get("/adv/all")).andExpect(status().isOk())
-//                .andExpect(jsonPath("$.size()", Matchers.equalTo(1)));
-//    }
+    @Test
+    public void whenGetAllAdvertisements_thenReturnAdvertisements() throws Exception {
+        // when && then
+        mockMvc.perform(get("/adv/all")).andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", Matchers.equalTo(3)));
+    }
 
-//    @Test
-//    @Sql("/sql/controller/advertisement.sql")
-//    public void whenGetAdvertisementsByCriteria_thenOkResponse() throws Exception {
-//        // given
-//        var criteria = AdvertisementSearchCriteriaDto.builder()
-//                .technologyName(TechnologyEnum.JAVA).build();
-//        var criteriaJson = toJson(criteria);
-//
-//        // when && then
-//        mockMvc.perform(post("/adv/search?page=0&size=10&sort=id,DESC")
-//                        .content(criteriaJson)
-//                        .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.size()", Matchers.equalTo(2)));
-//    }
+    @Test
+    @WithMockUser(username = "admin", password = "admin", roles = {"ADMIN"})
+    public void whenGetAdvertisementsByCriteria_thenOkResponse() throws Exception {
+        // given
+        var criteria = AdvertisementSearchCriteriaDto.builder()
+                .technologyName(TechnologyEnum.JAVA).build();
+        var criteriaJson = toJson(criteria);
+
+        // when && then
+        mockMvc.perform(post("/adv/search?page=0&size=10&sort=id,DESC")
+                        .content(criteriaJson)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", Matchers.equalTo(2)));
+    }
 
     @ParameterizedTest
     @ArgumentsSource(CriteriaProvider.class)
-    @Sql("/sql/controller/advertisement.sql")
     @WithMockUser(username = "admin", password = "admin", roles = {"ADMIN"})
     public void whenGetAdvertisementsByCriteriaParemetrized_thenOkResponse(TechnologyEnum technology,
                                                                            String city,
@@ -84,10 +87,9 @@ public class AdvertisementControllerIT {
         @Override
         public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) {
             return Stream.of(
-                    Arguments.of(TechnologyEnum.JAVA, null, null, 2)
-//                    ,
-//                    Arguments.of(null, "Nowogrodziec", null, 1),
-//                    Arguments.of(null, null, SeniorityEnum.MID, 1)
+                    Arguments.of(TechnologyEnum.JAVA, null, null, 2),
+                    Arguments.of(null, "Nowogrodziec", null, 1),
+                    Arguments.of(null, null, SeniorityEnum.MID, 1)
             );
         }
     }
